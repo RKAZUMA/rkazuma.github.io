77e2412e5fe5b5df41e286d6009c05a0854e636d
Ricardo Becerra <ingjaysonb@gmail.com>
2025-10-15 16:16:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Actualizar el año en el footer
+    const currentYearSpan = document.getElementById('current-year');
+    if (currentYearSpan) {
+        currentYearSpan.textContent = new Date().getFullYear();
+    }
+});
